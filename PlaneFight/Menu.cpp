d38b4ecbc580c573d14f4577b824a11e3bd7d371
Bowen Xiao <121174402+Bowie375@users.noreply.github.com>
2023-06-03 01:16:07 +0800

    Add files via upload

--- PlaneFight/Menu.cpp
+++ PlaneFight/Menu.cpp
@@ -6,6 +6,7 @@
 #include "qpixmap.h"
 #include "qpoint.h"
 #include "GameReview.h"
+//#include "Rule.h"
 #include "Ranking.h"
 #include <stdio.h>
 #include <iostream>
@@ -21,15 +22,15 @@ Menu::Menu(QWidget* parent) : QWidget(parent), ui(new Ui::MenuClass) {
 	ui->setupUi(this);
 
 	stackWidget = new QStackedWidget();
-	gameWidgets.push_back(new QWidget());                     // 0 interface for start menu
-	gameWidgets.push_back(new QWidget());                     // 1 interface for choosing mode
-	gameWidgets.push_back(new BattleField(nullptr, this));    // 2 interface for survive mode
-	gameWidgets.push_back(new QWidget());                     // 3 interface for choosing level mode
-	gameWidgets.push_back(new Ranking(nullptr, this));        // 4 interface for ranking
-	gameWidgets.push_back(new GameReview(nullptr, this));     // 5 interface for game review
-	gameWidgets.push_back(new QWidget());                     // 6 interface for rule
-	gameWidgets.push_back(new QWidget());                     // 7 interface for levels
-	gameWidgets.push_back(new QWidget());                     // 8 interface for levels
+	gameWidgets.push_back(new QWidget());				       //0 interface for start menu
+	gameWidgets.push_back(new QWidget());				       //1 interface for choosing mode
+	gameWidgets.push_back(new BattleField(nullptr, this));     //2 interface for survive mode
+	gameWidgets.push_back(new QWidget());			           //3 interface for choosing level mode
+	gameWidgets.push_back(new Ranking(nullptr,this));		   //4 interface for ranking
+	gameWidgets.push_back(new GameReview(nullptr, this));	   //5 interface for game review
+	gameWidgets.push_back(new QWidget());					   //6 interface for rule
+	gameWidgets.push_back(new QWidget());					   //7 interface for levels
+	gameWidgets.push_back(new QWidget());					   //8 interface for levels
 
 	for (int i = 0; i <= 1; i++) {
 		gameWidgets[i]->setAutoFillBackground(true);
@@ -40,18 +41,34 @@ Menu::Menu(QWidget* parent) : QWidget(parent), ui(new Ui::MenuClass) {
 		gameWidgets[i]->setPalette(palette);
 	}
 
+	QString button_style = "QPushButton{background-color:#000000;\
+										color: white; border-radius:10px ; border : 2px groove gray;\
+										border-style: outset;}"
+							"QPushButton:hover{background-color: white;color: black;}"
+							"QPushButton:pressed{background-color:rgb{85,170,255};\
+									border-style: inset;}";
+	
+	QString exit_button_style = "QPushButton{background-color:grey;\
+										color: white; border-radius:10px ; border : 2px groove gray;\
+										border-style: outset;}"
+								"QPushButton:hover{background-color: white;color: black;}"
+								"QPushButton:pressed{background-color:rgb{85,170,255};\
+									border-style: inset;}";
+
 	Title = new QLabel(gameWidgets[0]);
 	Title->setText("Plane Fight");
-	Title->setFont(QFont("consolas", 32));
-	Title->setGeometry(QRect(250, 100, 400, 150));
+	Title->setFont(QFont("Segoe Print", 48));
+	Title->setGeometry(QRect(200, 100, 400, 150));
+	Title->setStyleSheet("color:#ff0000");
 
 	for (int i = 0; i < 3; ++i) {
 		startButtons.push_back(new QPushButton(gameWidgets[0]));
 		startLabels.push_back(new QLabel(gameWidgets[0]));
 		startButtons[i]->setMouseTracking(true);
 		startLabels[i]->setText("");
-		startButtons[i]->setFont(QFont("consolas", 17));
-		startButtons[i]->setAutoFillBackground(true);
+		startButtons[i]->setFont(QFont("MV Boli", 20));
+		startButtons[i]->setStyleSheet(button_style);
+
 		switch (i) {
 			case 0:
 				startButtons[i]->setText("Start Now!");
@@ -77,37 +94,36 @@ Menu::Menu(QWidget* parent) : QWidget(parent), ui(new Ui::MenuClass) {
 
 	toFirstPage = new QPushButton(gameWidgets[1]);
 	toFirstPage->setText("<<Return");
-	toFirstPage->setFont(QFont("consolas", 15));
+	toFirstPage->setFont(QFont("MV Boli", 15));
 	toFirstPage->setGeometry(QRect(50, 700, 100, 50));
 	toFirstPage->setAutoFillBackground(true);
+	toFirstPage->setStyleSheet(exit_button_style);
 	connect(toFirstPage, &QPushButton::clicked, this, &Menu::ToFirstPage);
 
 	for (int i = 0; i < 3; ++i) {
-		gameModes.push_back(new QCommandLinkButton(gameWidgets[1]));
+		gameModes.push_back(new QPushButton(gameWidgets[1]));
 		modeLabels.push_back(new QLabel(gameWidgets[1]));
-		gameModes[i]->setFont(QFont("consolas", 17));
+		gameModes[i]->setFont(QFont("MV Boli", 20));
 		gameModes[i]->setMouseTracking(true);
+		gameModes[i]->setStyleSheet(button_style);
 		modeLabels[i]->setText("");
 		switch (i) {
 			case 0:
 				gameModes[i]->setText("Survive Mode");
-				gameModes[i]->setGeometry(QRect(290, 150, 191, 71));
-				gameModes[i]->setIcon(QIcon::fromTheme(tr("none")));
-				modeLabels[i]->setGeometry(QRect(220, 140, 65, 71));
+				gameModes[i]->setGeometry(QRect(290, 200, 191, 71));
+				modeLabels[i]->setGeometry(QRect(220, 200, 65, 71));
 				connect(gameModes[i], &QPushButton::clicked, this, &Menu::GameMode1);
 				break;
 			case 1:
-				gameModes[i]->setText(" level Mode");
-				gameModes[i]->setGeometry(QRect(290, 350, 191, 71));
-				gameModes[i]->setIcon(QIcon::fromTheme(tr("none")));
-				modeLabels[i]->setGeometry(QRect(220, 340, 65, 71));
+				gameModes[i]->setText("level Mode");
+				gameModes[i]->setGeometry(QRect(290, 400, 191, 71));
+				modeLabels[i]->setGeometry(QRect(220, 400, 65, 71));
 				connect(gameModes[i], &QPushButton::clicked, this, &Menu::GameMode2);
 				break;
 			case 2:
-				gameModes[i]->setText("  Rankings");
-				gameModes[i]->setGeometry(QRect(290, 550, 191, 71));
-				gameModes[i]->setIcon(QIcon::fromTheme(tr("none")));
-				modeLabels[i]->setGeometry(QRect(220, 540, 65, 71));
+				gameModes[i]->setText("Rankings");
+				gameModes[i]->setGeometry(QRect(290, 600, 191, 71));
+				modeLabels[i]->setGeometry(QRect(220, 600, 65, 71));
 				connect(gameModes[i], &QPushButton::clicked, this, &Menu::GameMode3);
 				break;
 			default: gameModes[i]->setText("bt"); break;
@@ -164,20 +180,19 @@ void Menu::ToFirstPage() {
 }
 
 void Menu::mouseMoveEvent(QMouseEvent* _event) {
-	int _x = _event->x(), _y = _event->y();
-	if (_x > startButtons[0]->geometry().left() && _x < startButtons[0]->geometry().right() &&
-	    stackWidget->currentIndex() == 0) {
-		if (_y > startButtons[0]->geometry().top() && _y < startButtons[0]->geometry().bottom()) {
+	int _x = _event->pos().x(), _y = _event->pos().y();
+	if (_x > startButtons[0]->geometry().left() && _x < startButtons[0]->geometry().right() && stackWidget->currentIndex() == 0) {
+		if (_y < startButtons[0]->geometry().bottom() && _y > startButtons[0]->geometry().top()) {
 			startLabels[0]->setPixmap(QPixmap(QString::fromUtf8(":/PlaneFight/img/planeIcon.svg")));
 			startLabels[1]->setPixmap(QPixmap("none"));
 			startLabels[2]->setPixmap(QPixmap("none"));
-		} else if (_y > startButtons[1]->geometry().top() &&
-		           _y < startButtons[1]->geometry().bottom()) {
+		}
+		else if (_y < startButtons[1]->geometry().bottom() && _y > startButtons[1]->geometry().top()) {
 			startLabels[0]->setPixmap(QPixmap("none"));
 			startLabels[1]->setPixmap(QPixmap(QString::fromUtf8(":/PlaneFight/img/planeIcon.svg")));
 			startLabels[2]->setPixmap(QPixmap("none"));
-		} else if (_y > startButtons[2]->geometry().top() &&
-		           _y < startButtons[2]->geometry().bottom()) {
+		}
+		else if (_y < startButtons[2]->geometry().bottom() && _y > startButtons[2]->geometry().top()) {
 			startLabels[0]->setPixmap(QPixmap("none"));
 			startLabels[1]->setPixmap(QPixmap("none"));
 			startLabels[2]->setPixmap(QPixmap(QString::fromUtf8(":/PlaneFight/img/planeIcon.svg")));
@@ -191,17 +206,18 @@ void Menu::mouseMoveEvent(QMouseEvent* _event) {
 		startLabels[1]->setPixmap(QPixmap("none"));
 		startLabels[2]->setPixmap(QPixmap("none"));
 	}
-	if (_x > gameModes[0]->geometry().left() && _x < gameModes[0]->geometry().right() &&
-	    stackWidget->currentIndex() == 1) {
-		if (_y > gameModes[0]->geometry().top() && _y < gameModes[0]->geometry().bottom()) {
+	if (_x > gameModes[0]->geometry().left() && _x < gameModes[0]->geometry().right() && stackWidget->currentIndex()==1) {
+		if (_y < gameModes[0]->geometry().bottom() && _y > gameModes[0]->geometry().top()) {
 			modeLabels[0]->setPixmap(QPixmap(QString::fromUtf8(":/PlaneFight/img/planeIcon.svg")));
 			modeLabels[1]->setPixmap(QPixmap("none"));
 			modeLabels[2]->setPixmap(QPixmap("none"));
-		} else if (_y > gameModes[1]->geometry().top() && _y < gameModes[1]->geometry().bottom()) {
+		}
+		else if (_y < gameModes[1]->geometry().bottom() && _y > gameModes[1]->geometry().top()) {
 			modeLabels[0]->setPixmap(QPixmap("none"));
 			modeLabels[1]->setPixmap(QPixmap(QString::fromUtf8(":/PlaneFight/img/planeIcon.svg")));
 			modeLabels[2]->setPixmap(QPixmap("none"));
-		} else if (_y > gameModes[2]->geometry().top() && _y < gameModes[2]->geometry().bottom()) {
+		}
+		else if (_y < gameModes[2]->geometry().bottom() && _y > gameModes[2]->geometry().top()) {
 			modeLabels[0]->setPixmap(QPixmap("none"));
 			modeLabels[1]->setPixmap(QPixmap("none"));
 			modeLabels[2]->setPixmap(QPixmap(QString::fromUtf8(":/PlaneFight/img/planeIcon.svg")));
